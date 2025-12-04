f5f2c0bbf31aa85b04860f45417e62482c0b0f25
Zyad <ziad.w.khedr@gmail.com>
2025-12-04 23:18:48 +0200

    first commit

---
+++ lib/liquid_navbar.dart
@@ -0,0 +1,8 @@
+library liquid_navbar;
+
+export 'src/bottom_nav_scaffold.dart';
+export 'src/navbar_widget.dart';
+export 'src/navbar_draggable_indicator.dart';
+export 'src/navbar_item_widget.dart';
+export 'src/navbar_background.dart';
+export 'src/navbar_providers.dart';
